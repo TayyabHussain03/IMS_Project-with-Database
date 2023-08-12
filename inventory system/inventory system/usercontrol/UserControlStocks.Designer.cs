69dd1477867fc86a7f463a26e15a13e9c213094f
TayyabHussain03 <tayyabfaisal106@gmail.com>
2023-08-12 17:44:45 +0500

    Stocks Searching added

--- inventory system/inventory system/usercontrol/UserControlStocks.Designer.cs
+++ inventory system/inventory system/usercontrol/UserControlStocks.Designer.cs
@@ -28,10 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            btnRefresh = new Button();
+            btnDelete = new Button();
+            dgvProducts = new DataGridView();
+            txtSearchProducts = new TextBox();
+            btnSearchProducts = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvProducts).BeginInit();
+            SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            btnRefresh.FlatAppearance.BorderSize = 0;
+            btnRefresh.Image = Properties.Resources.icons8_refresh_18;
+            btnRefresh.ImageAlign = ContentAlignment.MiddleLeft;
+            btnRefresh.Location = new Point(68, 160);
+            btnRefresh.Name = "btnRefresh";
+            btnRefresh.Padding = new Padding(4, 0, 0, 0);
+            btnRefresh.Size = new Size(80, 33);
+            btnRefresh.TabIndex = 30;
+            btnRefresh.Text = "      Refresh";
+            btnRefresh.UseVisualStyleBackColor = true;
+            btnRefresh.Click += btnRefresh_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.BackColor = Color.Red;
+            btnDelete.FlatAppearance.BorderSize = 0;
+            btnDelete.FlatStyle = FlatStyle.Flat;
+            btnDelete.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnDelete.ForeColor = Color.FromArgb(224, 224, 224);
+            btnDelete.Image = Properties.Resources.icons8_delete_16;
+            btnDelete.ImageAlign = ContentAlignment.MiddleLeft;
+            btnDelete.Location = new Point(613, 160);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Padding = new Padding(5, 0, 0, 0);
+            btnDelete.Size = new Size(85, 33);
+            btnDelete.TabIndex = 29;
+            btnDelete.Text = "     Delete";
+            btnDelete.TextAlign = ContentAlignment.MiddleLeft;
+            btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // dgvProducts
+            // 
+            dgvProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvProducts.BackgroundColor = Color.White;
+            dgvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProducts.Location = new Point(68, 199);
+            dgvProducts.Name = "dgvProducts";
+            dgvProducts.RowTemplate.Height = 25;
+            dgvProducts.Size = new Size(630, 345);
+            dgvProducts.TabIndex = 28;
+            // 
+            // txtSearchProducts
+            // 
+            txtSearchProducts.Font = new Font("Sitka Banner", 15.7499981F, FontStyle.Italic, GraphicsUnit.Point);
+            txtSearchProducts.Location = new Point(68, 90);
+            txtSearchProducts.Name = "txtSearchProducts";
+            txtSearchProducts.PlaceholderText = "Search Products by Name";
+            txtSearchProducts.Size = new Size(584, 34);
+            txtSearchProducts.TabIndex = 27;
+            // 
+            // btnSearchProducts
+            // 
+            btnSearchProducts.Cursor = Cursors.Hand;
+            btnSearchProducts.FlatStyle = FlatStyle.Flat;
+            btnSearchProducts.Image = Properties.Resources.icons8_search_24;
+            btnSearchProducts.Location = new Point(658, 90);
+            btnSearchProducts.Name = "btnSearchProducts";
+            btnSearchProducts.Size = new Size(40, 34);
+            btnSearchProducts.TabIndex = 26;
+            btnSearchProducts.UseVisualStyleBackColor = true;
+            btnSearchProducts.Click += btnSearchProducts_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Sitka Banner", 21.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkSlateGray;
+            label1.Location = new Point(342, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 42);
+            label1.TabIndex = 25;
+            label1.Text = "Stocks";
+            // 
+            // UserControlStocks
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnRefresh);
+            Controls.Add(btnDelete);
+            Controls.Add(dgvProducts);
+            Controls.Add(txtSearchProducts);
+            Controls.Add(btnSearchProducts);
+            Controls.Add(label1);
+            Name = "UserControlStocks";
+            Size = new Size(762, 660);
+            Load += UserControlStocks_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvProducts).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnRefresh;
+        private Button btnDelete;
+        private DataGridView dgvProducts;
+        private TextBox txtSearchProducts;
+        private Button btnSearchProducts;
+        private Label label1;
     }
 }
--- inventory system/inventory system/usercontrol/UserControlStocks.cs
+++ inventory system/inventory system/usercontrol/UserControlStocks.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,51 @@ namespace inventory_system.usercontrol
         public UserControlStocks()
         {
             InitializeComponent();
+        }
+
+        string connectionString = "Data Source=Desktop-SJVABES;Initial Catalog=dbIMS;Integrated Security=True";
+
+        private void FillStocksDataInGridView(string selectionQuery)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string selectStocksQuery = selectionQuery;
+
+                using (SqlCommand selectStocksCommand = new SqlCommand(selectStocksQuery, connection))
+                {
+                    using (SqlDataReader reader = selectStocksCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string productNumber = reader["Product_Number"].ToString();
+                            string productName = reader["Product_Name"].ToString();
+                            decimal sellingPrice = Convert.ToDecimal(reader["Selling_Price"]);
+                            decimal costingPrice = Convert.ToDecimal(reader["Costing_Price"]);
+                            int quantity = Convert.ToInt32(reader["Quantity"]);
+
+                            dgvProducts.Rows.Add(productNumber, productName, sellingPrice, costingPrice, quantity);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UserControlStocks_Load(object sender, EventArgs e)
+        {
+            FillStocksDataInGridView("Select * from Stocks");
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            FillStocksDataInGridView("Select * from Stocks");
+        }
+
+        private void btnSearchProducts_Click(object sender, EventArgs e)
+        {
+            string query = "Select * From Stocks where Product_Name like '%" + txtSearchProducts.Text + "%'";
+            FillStocksDataInGridView(query);
         }
     }
 }
